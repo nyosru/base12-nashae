74144c892ca11e9ac61879a54dadc4f0ecdf5d99
Сергей <1@php-cat.com>
2022-12-03 05:41:53 +0500

    1212

--- resources/js/views/pages/VitrinFormSend.vue
+++ resources/js/views/pages/VitrinFormSend.vue
@@ -207,7 +207,8 @@ export default {
       // sendToTelegramm(123123123);
 
       axios
-        .get("/api/sendOrder", {
+        .post("/api/sendOrder", {
+        // .get("/api/sendOrder", {
           goods: good.value,
           fio: fio.value,
           phone: phone.value
